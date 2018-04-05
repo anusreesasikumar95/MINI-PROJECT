6dfe7cc8116b6c4f47df8f723f2bc741e5ddcc47
Anusree Sasikumar <anusreesasikumar@mca.ajce.in>
2018-04-05 10:38:37 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Cake Delights | Services</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!--[if IE 8]><link rel="stylesheet" type="text/css" href="css/ie8.css"><![endif]-->
+<!--[if IE 7]><link rel="stylesheet" type="text/css" href="css/ie7.css"><![endif]-->
+<!--[if IE 6]><link rel="stylesheet" type="text/css" href="css/ie6.css"><![endif]-->
+</head>
+<body>
+<div id="header">
+  <div>
+    <div>
+      <div id="logo"> <a href="#"><img src="images/bakery_logo1.png" alt=""></a> </div>
+      <div>
+        <div> <a href="signup.php">My Account</a>  <a href="signin.php" class="last">Sign in</a> </div>
+       
+      </div>
+    </div>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="product.php">Today's Special</a></li>
+      <li><a href="about.php">About us</a></li>
+      <li class="current"><a href="services.php">Services</a></li>
+      
+      <li><a href="feedback.php">Feedback</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+    <div class="section"> <a href="#"><img src="images/wedding-cupcakes-small.jpg" alt=""></a> </div>
+  </div>
+</div>
+<div id="content">
+  <div id="services">
+    <div class="aside">
+      <h1>Our Services</h1>
+      <span><a href="catering.php">Catering</a></span>
+      <p>All of our platters are made with ingredients which are sourced on a daily basis and handled with care to produce both a satisfying and tasteful experience. Please browse through our Platter Catalogue to place your orders or call us directly on 0114401561 .</p>
+       <span><a href="party_home.php">Bulk/Party Orders</a></span>
+      <p>We also accept bulk or party orders including T parties ,marriage reception orders or any kind of parties..Please browse through our items and serve to your loved ones...we are here to help you!!!!</p>
+       <span><a href="viewcake.php">Custom made cakes</a></span>
+      <p>Cakes for any occasion Birthday Cakes ,Anniversary Cakes ,Wedding Cakes,Baby Shower Cakes...anything and everything as you wish in your favourite flavours,color and shape...Just a matter of within 24 hrs Delivery.</p>
+    </div>
+    <div class="section">
+      <h3><a href="book_table.php">Book A Table</a></h3>
+      <p>Enjoy your day with your special one with our tastily baked products....WE are here with our shp ..Just Come and join with Us .</p>
+      
+      <div id="visitshop">
+        <div>
+          <p><span>Sweets from the heart</span> Treat your loved ones</p>
+          <a href="#" class="visit">visit the shop</a> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="section">
+    <div>
+      <div class="aside">
+        <div>
+          <div> <b>Too <span>BUSY</span> to shop?</b> <a href="signin.php">Sign up for free</a>
+            <p>and we'll deliver it on your doorstep</p>
+          </div>
+        </div>
+      </div>
+      <div class="connect"> <span>Follow Us</span>
+        <ul>
+          <li><a href="#" class="facebook">Facebook</a></li>
+          <li><a href="#" class="twitter">Twitter</a></li>
+          <li><a href="#" class="subscribe">Subscribe</a></li>
+          <li><a href="#" class="flicker">Flicker</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div id="featured">
+    <ul>
+      <li class="first"> <a href="#"><img src="images/fruit-cake.jpg" alt=""></a>
+        <h2><a href="#">Tips on how to preserve Cakes</a></h2>
+         <p>To keep cakes for longer than a week, try freezing them. Frosted and Un-Cut Cake - The frosting actually acts like plastic wrap here, protecting the cake from air and moisture in the environment.</p>
+        
+       
+      </li>
+      <li> <a href="#"><img src="images/italian.jpg" alt=""></a>
+        <h2><a href="#">Menu of the day: Italian</a></h2>
+        <p>Italian food is one of the few global cuisines that Indians are obsessed with. It regularly features on the dining table in most urban Indian households.</p>
+      </li>
+      <li> <a href="#"><img src="images/fruit.jpg" alt=""></a>
+        <h2><a href="#">Fruit menu for your diet</a></h2>
+        <p>An all-fruit diet, also called fruitarian diet, comprises acid fruits, such as oranges and grapefruits;, sub-acid fruits, such as apples, cherries and berries; sweet fruits, such as grapes and melons;, oily fruits.</p>
+      </li>
+      <li> <a href="#"><img src="images/desserts.jpg" alt=""></a>
+        <h2><a href="#">Desserts for every occassion</a></h2>
+        <p>The term "dessert" can apply to many confections, such as cakes, tarts, cookies, biscuits, gelatins, pastries, ice creams, pies, puddings, custards, and sweet soups. Fruit is also commonly found in dessert courses because of its naturally occurring sweetness.</p>
+      </li>
+    </ul>
+  </div>
+  <div id="navigation">
+    
+  </div>
+</div>
+</body>
+</html>
